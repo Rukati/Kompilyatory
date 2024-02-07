be8526b9dd43e660ff0df9678b0a042ea98a05f3
mega- <mega-@RUKATI>
2024-02-08 00:40:32 +0300

    Добавьте файлы проекта.

---
+++ MyVisitor.cs
@@ -0,0 +1,37 @@
+﻿using Antlr4.Runtime;
+using Antlr4.Runtime.Misc;
+using Antlr4.Runtime.Tree;
+using System;
+
+namespace Kompilyatory
+{
+    internal class MyVisitor : ExprBaseVisitor<IParseTree>
+    {
+        public override IParseTree VisitInitialization([NotNull] ExprParser.InitializationContext context)
+        {
+            var name = context.ID().GetText();
+            var type = context.TYPE().GetText();
+            var value = context.NUMBER() == null ? null : context.NUMBER().GetText();
+
+            Console.WriteLine($"+-- Announcement (type: {type}, ID: {name})\n|\t|\n|\t+-- value ({value})");
+            return this.VisitChildren(context);
+        }
+
+    /*    public override IParseTree ([NotNull] ExprParser.InitializationContext context)
+        {
+            Console.WriteLine($"|\t\t|+-- type--- {context.GetText()}");
+            return this.VisitChildren(context);
+        }*/
+
+        public override IParseTree VisitProg([NotNull] ExprParser.ProgContext context)
+{
+            Console.WriteLine("Prog\n|");
+            return this.VisitChildren(context);
+        }
+
+        public override IParseTree VisitStat([NotNull] ExprParser.StatContext context)
+        {
+            return this.VisitChildren(context);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using Antlr4.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Antlr4.Runtime.Tree;
+
+namespace Kompilyatory
+{
+    class Program
+    {
+        static void KompFilesG4()
+        {
+            ProcessStartInfo psi;
+            psi = new ProcessStartInfo("cmd", @"/c java -jar antlr-4.9.3-complete.jar -Dlanguage=CSharp -visitor Expr.g4");
+            Process.Start(psi);
+        }
+        static void Main(string[] args)
+        {
+
+            AntlrFileStream antlrInputStream = new AntlrFileStream("lang.txt",Encoding.UTF8) ;
+
+            ExprLexer lexer = new ExprLexer(antlrInputStream);
+
+            CommonTokenStream commonToken = new CommonTokenStream(lexer);
+
+            ExprParser parser = new ExprParser(commonToken);
+
+            IParseTree tree = parser.prog();
+            
+            MyVisitor visitor = new MyVisitor();
+            visitor.Visit(tree);
+           
+            Console.WriteLine(tree.ToStringTree());
+        }
+    }
+}
